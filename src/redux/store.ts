beda192be4d0e877c5f97966dfc776435f81653d
Annarchive <ytpt@mail.ru>
2023-07-06 15:11:15 +0400

    Added checkbox reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,11 +3,14 @@ import userScoreReducer from "./reducers/userScoreReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import questionReducer from "./reducers/questionsReducer";
 import startQuizReducer from "./reducers/startQuizReducer";
+import checkboxReducer from "./reducers/checkboxReducer";
 
 const rootReducer = combineReducers({
     userScore: userScoreReducer,
     questions: questionReducer,
-    startQuiz: startQuizReducer,
+    isGameStarted: startQuizReducer,
+    isCheckboxValid: checkboxReducer,
+
 })
 
 export type RootState = ReturnType<typeof rootReducer>;
