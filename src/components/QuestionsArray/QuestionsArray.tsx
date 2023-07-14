153440d0e405cc9d79d17b8ba2228b6e80a3b4d0
Annarchive <ytpt@mail.ru>
2023-07-15 01:52:12 +0400

    Added timer function, handleSelectAnswer function, SelectedAnswers type

--- src/components/QuestionsArray/QuestionsArray.tsx
+++ src/components/QuestionsArray/QuestionsArray.tsx
@@ -1,43 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, FC } from "react";
 import QuestionCard from "../QuestionCard/QuestionCard";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { shuffleArray } from "../../utils";
 import PaginationButton from "../PaginationButton/PaginationButton";
-import ResultsButton from "../ResultsButton/ResultsButton";
+import Results from "../Results/Results";
 import LogoutButton from "../LogoutButton/LogoutButton";
-import { handleStartQuiz, resetUserScore} from "../../redux/actions";
+import { handleScoreShown, handleStartQuiz, handleTimerActive, resetUserScore } from "../../redux/actions";
 
 type Props = {
     newQuestions: any;
-    isClicked: boolean;
-    setIsClicked: (boolean) => void;
 }
 
-const QuestionsArray: React.FC<Props> = ({
+type SelectedAnswers = {
+    [questionNumber: number]: string | null;
+}
+
+const QuestionsArray: FC<Props> = ({
     newQuestions,
-    isClicked,
-    setIsClicked,
 }) => {
 
+    const dispatch = useDispatch();
     const userScore = useSelector((state: RootState) => state.userScore);
     const userAuthStatus = useSelector((state: RootState) => state.userStatus.user_auth);
+    const isGameStarted = useSelector((state: RootState) => state.isGameStarted.is_game_started);
+    const activeTimer = useSelector((state: RootState) => state.isTimerActive.is_timer_active);
+    const isScoreShown = useSelector((state: RootState) => state.isScoreShown.is_score_shown);
 
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        const startNewGame = () => {
-            dispatch(handleStartQuiz(true));
-            dispatch(resetUserScore(0));
-        }
-        startNewGame();
-    }, []);
-
+    const [selectedAnswers, setSelectedAnswers] = useState<SelectedAnswers>({});
+    const [isTimeExpired, setIsTimeExpired] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
     const totalQuestionsCount = newQuestions.length;
     const [page, setPage] = useState(1);
-    const limit = 5;
-    const startIndex = (page - 1) * limit;
-    const endIndex = startIndex + limit;
+    const limit = 5, startIndex = (page - 1) * limit, endIndex = startIndex + limit;
+
+    const startNewGame = () => {
+        setIsClicked(false);
+        setIsTimeExpired(false);
+        setSelectedAnswers({});
+        dispatch(resetUserScore(0));
+        dispatch(handleStartQuiz(true));
+        dispatch(handleTimerActive(true));
+        dispatch(handleScoreShown(false));
+        timer();
+    }
+
+    useEffect(() => {
+        startNewGame();
+    }, []);
 
     const handlePrevPage = () => {
         setPage((prevPage) => prevPage - 1);
@@ -57,10 +67,39 @@ const QuestionsArray: React.FC<Props> = ({
             ])
         }));
 
+   let seconds: number = 0;
+   let interval: ReturnType<typeof setTimeout>;
+
+   const timer = (minutes: number = 1) => {
+       seconds = minutes * 60 || 0;
+       dispatch(handleTimerActive(true));
+
+       interval = setInterval(() => {
+           seconds--;
+           console.log(seconds);
+
+           if (!seconds) {
+               clearInterval(interval);
+               dispatch(handleTimerActive(false));
+               dispatch(handleScoreShown(true));
+               setIsTimeExpired(true);
+               alert("Время вышло :(");
+           }
+       }, 1000);
+   }
+
+    const handleSelectAnswer = (questionNumber: number, answer: string) => {
+        setSelectedAnswers((prevSelectedAnswers) => ({
+            ...prevSelectedAnswers,
+            [questionNumber]: answer,
+        }));
+    };
+
     return (
         <>
+            {/*Questions*/}
             {
-                userAuthStatus
+                isGameStarted
                     && shuffledQuestions.map((question, index) => (
                         <QuestionCard
                             key={ index }
@@ -73,30 +112,32 @@ const QuestionsArray: React.FC<Props> = ({
                             right={ question.correct_answer }
                             isClicked={ isClicked }
                             setIsClicked={ setIsClicked }
+                            isTimeExpired={ isTimeExpired }
+                            selectedAnswer={ selectedAnswers[index + startIndex] || null }
+                            setSelectedAnswer={ (answer) => handleSelectAnswer(index + startIndex, answer) }
                         />
                     ))
             }
+            {/*Pagination*/}
             {
-                userAuthStatus && newQuestions.length > endIndex
-                    && <PaginationButton
-                        onClick={ handleNextPage }
-                        value="Следующая страница"
-                    />
+                isGameStarted && newQuestions.length > endIndex && !isScoreShown
+                    && <PaginationButton onClick={ handleNextPage } value="Следующая страница" />
                     || page > 1
-                        && <PaginationButton
-                            onClick={ handlePrevPage }
-                            value="Предыдущая страница"
-                        />
+                        &&<PaginationButton onClick={ handlePrevPage } value="Предыдущая страница" />
             }
+            {/*Timer*/}
             {
-                userAuthStatus
-                    && <ResultsButton
-                        userScore={ userScore.user_score }
-                        totalQuestionsCount={ totalQuestionsCount }
-                        setIsClicked={ setIsClicked }
+                isGameStarted && !activeTimer
+                    && <Results
+                            userScore={ userScore.user_score }
+                            totalQuestionsCount={ totalQuestionsCount }
+                            startNewGame={ startNewGame }
                     />
             }
-            { userAuthStatus && <LogoutButton value="Выход" /> }
+            {/*Logout*/}
+            {
+                isGameStarted && userAuthStatus && <LogoutButton value="Выход" />
+            }
         </>
     )
 };
