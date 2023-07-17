5d63d03ffcdcf9ceef98a30273cd502b8af593e9
Annarchive <ytpt@mail.ru>
2023-07-18 00:15:29 +0400

    Deleted unused styles

--- src/components/QuestionCard/QuestionCard.styles.ts
+++ src/components/QuestionCard/QuestionCard.styles.ts
@@ -1,23 +1,9 @@
 import styled from "styled-components";
 
-type ButtonWrapperProps = {
-    correct: boolean;
-    userClicked: boolean;
-}
-
 export const Wrapper = styled.div`
     width: 800px;  
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;  
-`;
-
-export const ButtonWrapper = styled.div<ButtonWrapperProps>`
-    width: max-content;
-    display: flex; 
-    justify-content: space-between;
-    flex-direction: row;
-    column-gap: 5px;
-    transition: all 0.3s ease;
 `;
