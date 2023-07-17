947bb1e687355026ed0f4dbdbe7cc6a232f6ae02
Annarchive <ytpt@mail.ru>
2023-07-18 00:11:17 +0400

    Refactored code

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle} from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import BGImage from "./assets/flags.jpg";
 
 export const GlobalStyle = createGlobalStyle`
