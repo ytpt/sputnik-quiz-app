11695576cf924350793d74e766c900d1b00216b9
Annarchive <ytpt@mail.ru>
2023-07-19 01:14:28 +0400

    Added AppWrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
+import { AppWrapper } from "./AppWrapper";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-    <Provider store={ store }>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <App /> } />
-                <Route path="*" element={ <NotFoundPage /> } />
-            </Routes>
-        </BrowserRouter>
-    </Provider>
-);
+root.render(<AppWrapper />);
