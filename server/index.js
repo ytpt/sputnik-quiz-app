ba6b9ebb5b27655a37060a8e2403175361585924
Annarchive <ytpt@mail.ru>
2023-07-22 01:18:11 +0400

    Added cors hack №4

--- server/index.js
+++ server/index.js
@@ -11,10 +11,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: process.env.CLIENT_URL,
+// }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use("/api", router);
 app.use(errorMiddleware);
 
