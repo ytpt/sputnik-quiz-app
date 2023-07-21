ffdacbb5ef2de31875fe3f9c1196f6414bf3bf46
Annarchive <ytpt@mail.ru>
2023-07-21 23:01:46 +0400

    Refactored API_URL

--- src/http/index.ts
+++ src/http/index.ts
@@ -3,7 +3,7 @@ import { AuthResponse } from "../models/response/AuthResponse";
 import { handleErrorMessage } from "../redux/actions";
 import { useDispatch } from "react-redux";
 
-export const API_URL = `https://quiz-app-server-delta.vercel.app/api`;
+export const API_URL = `https://localhost:5000/api`;
 export const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL,
